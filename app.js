63f2a235bd65fecfb542201c7f5624024e2805d0
marc-dsalab <marc.jansen@hs-ruhrwest.de>
2014-11-17 14:58:48 +0100

    added modules infrastructure

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var waf = require('./express-waf');
 
+waf.addModule('./testModule', {}, function(error) {
+    console.log(error);
+});
+
 app.use(waf.check);
 app.get('/', function(req, res) {
     res.status(200).end('Hello world!');
