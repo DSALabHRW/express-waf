cd48862e3d09d090ccc717c09ec49c74b595fbd2
marc-dsalab <marc.jansen@hs-ruhrwest.de>
2014-11-17 14:58:24 +0100

    added modules infrastructure

--- express-waf.js
+++ express-waf.js
@@ -1,6 +1,23 @@
-module.exports.check = function(req, res, cb) {
-    console.log(req.url);
-    if (cb) {
-        cb();
-    }
-};
+(function() {
+    var modules = [];
+
+    module.exports.addModule = function (moduleName, config, callback) {
+        var firewallModule = require(moduleName);
+
+        if (firewallModule.check && firewallModule.init) {
+            firewallModule.init(config);
+            modules.push(firewallModule);
+        } else {
+            callback(moduleName + ' does not define a check and an init function!');
+        }
+    };
+
+    module.exports.check = function (req, res, cb) {
+        for (var m in modules) {
+            modules[m].check(req, res);
+        }
+        if (cb) {
+            cb();
+        }
+    };
+})();
