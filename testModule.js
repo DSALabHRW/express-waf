86841d026dabe2d3fb959d7b6654eb27f8e9d022
marc-dsalab <marc.jansen@hs-ruhrwest.de>
2014-11-19 11:59:19 +0100

    added the callback at the end of the check method

--- testModule.js
+++ testModule.js
@@ -1,13 +1,18 @@
 (function() {
 
     var _config;
+    var _blocker;
 
-    function TestModule(config) {
+    function TestModule(config, blocker) {
         _config = config;
+        _blocker = blocker;
     };
 
     TestModule.prototype.check = function(req, res, cb) {
         console.log(req.url);
+        if (cb) {
+            cb();
+        }
     };
 
     module.exports = TestModule;
