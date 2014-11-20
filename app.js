ce9a8b668d33b0fae98be278ffb57b34d080c708
marc-dsalab <marc.jansen@hs-ruhrwest.de>
2014-11-20 20:09:40 +0100

    changes necessary for passing the tests of the xss module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 var ExpressWAF = require('./express-waf');
 var waf = new ExpressWAF({
@@ -12,8 +13,21 @@ waf.addModule('./XSSModule', {}, function(error) {
     console.log(error);
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(waf.check);
 app.get('/', function(req, res) {
+    res.status(200).end('Hello world!');
+});
+app.delete('/', function(req, res) {
+    res.status(200).end('Hello world!');
+});
+app.post('/', function(req, res) {
+    res.status(200).end('Hello world!');
+});
+app.put('/', function(req, res) {
     res.status(200).end('Hello world!');
 });
 
